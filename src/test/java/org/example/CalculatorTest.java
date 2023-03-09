fba1881d96f651750cffc0f4ee1f1ef3c3a593ba
ms0130k <minseok0130@gmail.com>
2023-03-09 12:47:58 +0900

    사칙연산 계산기 및 테스트 코드 작성

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 
 /**
  * 간단한 사칙연산을 할 수 있다.
@@ -19,13 +19,13 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
  */
 public class CalculatorTest {
     @ParameterizedTest
-    @MethodSource("formulaAndResult")
-    void 사칙연산(double operand1, double operand2, String operator, double result) {
-        double calculateResult = Calculator.calculate(operand1, operand2, operator);
-        assertThat(calculateResult).isEqualTo(result);
+    @MethodSource("사칙연산_인자와_결과")
+    void 사칙연산(final double operand1, final double operand2, final String operator, final double expected) {
+        double result = Calculator.calculator(operand1, operand2, operator);
+        assertThat(result).isEqualTo(expected);
     }
 
-    private static Stream<Arguments> formulaAndResult() {
+    private static Stream<Arguments> 사칙연산_인자와_결과() {
         return Stream.of(
                 arguments(1, 2, "+", 3),
                 arguments(1, 2, "-", -1),
@@ -35,8 +35,23 @@ public class CalculatorTest {
     }
 
     @Test
-    void 나누기0() {
-        assertThatThrownBy(() -> Calculator.calculate(1, 0, "/")).isInstanceOf(IllegalArgumentException.class);
+    void 나누기0이면_예외발생() {
+        try {
+            Calculator.calculator(1, 0, "/");
+            fail("0으로 나누었으나 예외 발생하지 않음");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).isEqualTo(Calculator.CANNOT_DIVIDE_0);
+        }
+    }
+
+    @Test
+    void 사칙연산_외_입력시_예외발생() {
+        try {
+            Calculator.calculator(1, 0, "!");
+            fail("사칙연산이 아닌 기호가 입력되었으나 예외 발생하지 않음");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).isEqualTo(Calculator.IS_NOT_ARITHMETIC_OPERATOR);
+        }
     }
 }
 
